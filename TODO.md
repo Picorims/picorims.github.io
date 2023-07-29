27d1c2833ec25af3d95cba25c6d1de535ddce6bf
Picorims <picorims.contact@gmail.com>
2023-07-29 23:50:07 +0200

    add wav2bar description, publish website, add todo

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- tags for team and university projects
+- year of project
+- this website as project
--- src/routes/development/project/wav2bar/+page.svelte
+++ src/routes/development/project/wav2bar/+page.svelte
@@ -1,36 +1,41 @@
 <script lang="ts">
-	import ProjectHeader from "$lib/ProjectHeader.svelte";
-	import ProjectDetails from "$lib/ProjectDetails.svelte";
+	import ProjectHeader from '$lib/ProjectHeader.svelte';
+	import ProjectDetails from '$lib/ProjectDetails.svelte';
 
-	import { TechLists } from "$lib/techlists";
-    import w2bThumb from '$lib/assets/project_thumbnails/wav2bar_background.jpg';
+	import { TechLists } from '$lib/techlists';
+	import w2bThumb from '$lib/assets/project_thumbnails/wav2bar_background.jpg';
 </script>
 
-<ProjectHeader title="Wav2Bar" background={w2bThumb}></ProjectHeader>
+<ProjectHeader title="Wav2Bar" background={w2bThumb} />
 
 <ProjectDetails techList={TechLists.wav2bar} href="https://picorims.github.io/wav2bar-website/">
-    <span slot="strengths">
-        <li>published</li>
-        <li>positive feedback from users and small activity</li>
-        <li>automated builds</li>
-    </span>
-    <span slot="weaknesses">
-        <li>architecture and tech debt</li>
-        <li>performance</li>
-        <li>lack of unit tests</li>
-    </span>
-    <span slot="explored">
-        <li>native web app</li>
-        <li>canvas and audio processing</li>
-        <li>custom components in pure HTML5 (standard API)</li>
-        <li>states</li>
-        <li>user interface</li>
-        <li>CI / CD</li>
-    </span>
+	<span slot="strengths">
+		<li>published</li>
+		<li>positive feedback from users and small activity</li>
+		<li>automated builds</li>
+	</span>
+	<span slot="weaknesses">
+		<li>architecture and tech debt</li>
+		<li>performance</li>
+		<li>lack of unit tests</li>
+	</span>
+	<span slot="explored">
+		<li>native web app</li>
+		<li>canvas and audio processing</li>
+		<li>custom components in pure HTML5 (standard API)</li>
+		<li>states</li>
+		<li>user interface</li>
+		<li>CI / CD</li>
+	</span>
 </ProjectDetails>
 
 <section>
-    <p>
-        Duis duis id elit ea. Pariatur do aliquip voluptate ea voluptate. Ea et officia sunt incididunt quis enim proident deserunt culpa excepteur deserunt tempor consequat et. Ullamco dolor quis esse occaecat Lorem minim elit exercitation Lorem qui enim excepteur commodo. Exercitation in occaecat amet officia sit fugiat sunt enim dolor voluptate fugiat labore non aute.
-    </p>
+	<p>
+		The idea of Wav2Bar came a few months after the discontinuation of Sonic Candle. It was a
+		similar tool that allowed you to make and export basic audio visualization. I always felt
+		limited, and couldn't imagine the only open source option I knew and enjoyed would die. So, I
+		tried my best to take what made Sonic Candle a great tool, and focused on the ability to add
+		more customization. While being a project on my spare time, I still try to provide something as
+		professional as possible. I am also open to suggestions.
+	</p>
 </section>
