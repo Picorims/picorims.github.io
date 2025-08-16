058e41b25163ec7551262b777ae4a0b5418fd819
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-08-16 18:30:19 +0200

    ✨ feat(website): improve body background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,4 +15,12 @@
         /*make footer stick to bottom of the page if needed*/
         flex: 1;
     }
+
+    :global(body) {
+        background-color: var(--main-black);
+        background-image: 
+            repeating-linear-gradient(60deg, transparent, var(--main-blue-transparent-very-light) 1100px, transparent 2200px),
+            repeating-linear-gradient(30deg, transparent, var(--main-purple-transparent-very-light) 800px, transparent 1600px),
+            repeating-linear-gradient(45deg, transparent, var(--main-pink-transparent-very-light) 1300px, transparent 2600px);
+    }
 </style>
