9188ed80857b0ba21985f0e61c756969b739d4b1
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-03-04 17:06:11 +0100

    ✨ feat(development): add tech filters

--- src/lib/ProjectList.svelte
+++ src/lib/ProjectList.svelte
@@ -3,6 +3,7 @@
 	import { ProjectInfo, type ProjectCategory } from "./project_info";
 	import Project from "./Project.svelte";
 	import { Tag } from "./TagList.svelte";
+	import { Tech, techList } from "./TechList.svelte";
 
     interface Props {
         category: ProjectCategory;
@@ -10,12 +11,21 @@
 
     let { category }: Props = $props();
     let toggledTags = $state<Tag[]>([]);
+    let toggledTechs = $state<string[]>([]);
 
     function toggleTag(tag: Tag) {
         if (toggledTags.includes(tag)) {
             toggledTags = toggledTags.filter(t => t !== tag);
         } else {
             toggledTags.push(tag);
+        }
+    }
+
+    function toggleTech(tech: string) {
+        if (toggledTechs.includes(tech)) {
+            toggledTechs = toggledTechs.filter(t => t !== tech);
+        } else {
+            toggledTechs.push(tech);
         }
     }
 </script>
@@ -33,6 +43,28 @@
                 {/each}
             </div>
         </details>
+
+        <details>
+            <summary>Filter by technologies:</summary>
+            <div class="tag-list">
+                {#each Object.values(Tech).filter(t => typeof t === "string").sort() as tech}
+                    <!--
+                        Yes this is ugly but I still prefer TypeScript enums over string literals in this specific context.
+                        Maybe I'll change of mind in the future but for now it works and is constrained to this page.
+                     -->
+
+                    {@const techKey = Tech[tech as unknown as Tech] as unknown as Tech}
+
+                    <label class="togglable-tech" style="--tech-color: {techList[techKey].color}">
+                        <input type="checkbox" name={tech} value={tech} onchange={() => toggleTech(tech)} />
+                        {tech}
+                        <span class="icon" style:fill={techList[techKey].color}>
+                            {@html techList[techKey].icon}
+                        </span>
+                    </label>
+                {/each}
+            </div>
+        </details>
     {/if}
 </div>
 
@@ -40,7 +72,11 @@
 <div class="projects-container">
     {#each Object.keys(ProjectInfo) as key}
         {#if ProjectInfo[key].categories.includes(category)}
-            {#if toggledTags.length === 0 || toggledTags.every(tag => ProjectInfo[key].tags.includes(tag))}
+            {#if (toggledTags.length === 0 || toggledTags.every(tag => ProjectInfo[key].tags.includes(tag)))
+                && (toggledTechs.length === 0 || toggledTechs.every(tech => {
+                    const techKey = Tech[tech as unknown as Tech] as unknown as Tech;
+                    return ProjectInfo[key].tech.includes(techKey);
+                }))}
                 <Project
                     href={ProjectInfo[key].href}
                     shadowColor={ProjectInfo[key].shadowColor}
@@ -57,9 +93,9 @@
     details {
         border: 1px solid var(--main-purple);
         border-radius: var(--main-border-radius);
-        width: fit-content;
         overflow: hidden;
         box-shadow: 0 4px 12px var(--main-purple-transparent);
+        width: 100%;
     }
     /*Yes I know but native CSS is still relatively recent
     and I don't want this to break.*/
@@ -75,8 +111,10 @@
     div.filters {
         margin-bottom: 50px;
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
+        gap: 20px;
     }
     div.projects-container {
         display: flex;
@@ -84,16 +122,69 @@
 		justify-content: center;
 		gap: 50px;
     }
-    label.togglable-tag {
+
+    div.tag-list {
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    }
+
+    label.togglable-tech, label.togglable-tag {
         display: inline-flex;
         align-items: center;
         gap: 0.1em;
         margin-right: 15px;
+        margin-bottom: 10px;
         font-weight: bold;
+        cursor: pointer;
+    }
+
+    label.togglable-tag {
         color: var(--main-blue);
         border: 1px solid var(--main-blue);
         border-radius: var(--subtle-border-radius);
         padding: 0.1em 0.2em;
         background-color: var(--main-black-transparent);
     }
+    label.togglable-tag:hover {
+        background-color: var(--main-blue-transparent);
+    }
+    label.togglable-tag:has(input:checked) {
+        background-color: var(--main-blue);
+        color: var(--main-black);
+    }
+
+    label.togglable-tech {
+        color: oklch(from var(--tech-color, var(--main-white)) calc(l + 0.5) c h);
+        /* border: 1px solid oklch(from var(--tech-color, var(--main-white)) calc(l - 0.15) c h); */
+        border: 1px solid oklch(from var(--main-black) calc(l + 0.2) c h);
+        border-radius: var(--subtle-border-radius);
+        padding: 0.2em 0.4em;
+        background-color: var(--main-black);
+    }
+    label.togglable-tech:has(input:checked) {
+        background-color: var(--main-blue);
+        color: var(--main-black);
+    }
+
+    input[type="checkbox"] {
+        width: 16px;
+        height: 16px;
+        accent-color: var(--main-white);
+    }
+
+    span.icon {
+        display: inline-block;
+        width: 1.25em;
+        height: 1.25em;
+        margin: 0 0.25em;
+        margin-left: 0.5em;
+    }
+
+    label.togglable-tech:has(input:checked) span.icon {
+        background-color: var(--main-black);
+        box-shadow: 0 0 0 3px var(--main-black);
+        border-radius: var(--subtle-border-radius);
+    }
+
 </style>
